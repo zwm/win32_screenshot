10f3c78618894e2fa3e9a4307c2e0890e599e0f9
zwm <mingforregister@163.com>
2016-12-06 18:25:11 +0800

    20161206

--- src/win32_test1/win32_test1.cpp
+++ src/win32_test1/win32_test1.cpp
@@ -41,12 +41,25 @@
  *      (3) 消息循环
  *      (4) 编写窗口消息处理函数
  * 8. About hInstance?
+ *      一、
  *      也对，也不对，HINSTANCE是进程实例句柄，用来标识每个进程的，不过，这???
  *      参数并不是系统传给WinMain的，对于每一个Windows下的，C/C++程序，在编译
  *      时都会加上入口点函数，对Win32程序会加上函数：WinMainCRTStartup，在这个
  *      函数中通过调用 GetModuleHandle(NULL)来获得这个句柄，然后传给WinMain中???
  *      你在程序中就可以用它了，当然你不想用WinMain中传过来的参数，直接调用
  *      GetModuleHandle(NULL)也可以???
+ *      二、
+ *      hInstance是程序的当前实例的句柄。在Windows这样的多任务操作系统中，一个
+ *      程序可以同时运行多个实例。不同的实例间需要彼此区别，句柄就是干这个的。
+ *      hInstance是操作系统分配给程序自身实例的句柄.句柄是用来标识实例的,句柄是
+ *      实例指针的索引. 通过句柄能找到实例的地址.
+ *      三、
+ *      实际上HINSTANCE并非纯粹意义上的句柄（HANDLE），因为它实际上是由物理PE文
+ *      件映射到WINDOWS进程的虚拟模块首地址，这个地址上包含了PE头信息和PE文件映
+ *      射后的具体内容，微软将之作为句柄来体现在原因在于PE头过于复杂，使用者直接
+ *      对PE结构进行访问不安全，这种编程手法在C语言编程中很常见，对于使用者而言，
+ *      HINSTANCE的意义在于对WINDOWS资源进行访问，也可以将它作为HMODULE来访问模
+ *      块的导出函数等等。
  * 20161123 Study Contents
  * 9. What's HINSTANCE?
  *      -- Code Related:
@@ -60,7 +73,18 @@
  *      #define UNREFERENCED_PARAMETER(P)          (P)
  *      -- Just trick complier to make unused parameter seems to be used!!
  *      -- To avoid generate unwanted warnings
- *
+ * 20161206
+ * 11. What's LoadString()?
+ *      在编写国际化应用程序里，经常使用不同的字符串。比如中文菜单里叫“文件”，而
+ *      在英语里叫“File”。软件功能一样，界面显示文字不一样。为方便这种软件开发，在
+ *      Windows里经常使用的方法就是替换掉显示的字符串。这个功能可以通过函数LoadString
+ *      从不同的资源里加载不同的字符串来实现。其实所有可变的字符串，都可以使用函
+ *      数LoadString从资源里加载字符串显示。
+ * 12. What's type of ATOM?
+ *      typedef unsigned short      WORD;
+ *      typedef WORD                ATOM;
+ * 13. What's function LoadAccelerators(__in_opt HINSTANCE hInstance, __in LPCSTR ipTableName);
+ *      添加应用程序快捷键。
  *
  *
 ******************************************************************************/
